1b0702f91531c9638fb376ec7c73258d602e0e68
Claudio Spizzi Admin <adm-csp@arcdom.local>
2020-05-08 01:29:57 +0200

    Update run file

--- windows-ltsc2019/run.ps1
+++ windows-ltsc2019/run.ps1
@@ -1,4 +1,5 @@
 # Stop, remove, rebuild and start the containers
 docker-compose stop
 docker-compose rm -f
-docker-compose up -d --build
+#docker volume rm windows-ltsc2019_app_database
+docker-compose up -d --build3s
